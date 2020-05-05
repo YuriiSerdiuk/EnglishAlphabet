948f346b80967d0fd290a75ad060f29fe6a4b81c
Nik Prutkovskiy <n.prutkovskiy@gmail.com>
2020-05-05 19:37:09 +0300

    x2

--- src/index.js
+++ src/index.js
@@ -6,10 +6,7 @@ import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <App />
-     */}
-
-    <p>test</p>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
