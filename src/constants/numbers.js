14c4a04d639744e9962cf09a1ecbacd55cc6d8fb
Yura <cb8593bc@gmail.com>
2020-05-16 12:54:53 +0300

    add color

--- src/constants/numbers.js
+++ src/constants/numbers.js
@@ -5,103 +5,103 @@ export const numbers = [
   },
   {
     letter: "2",
-    color: "#F06543",
+    color: "#F99B28",
   },
   {
     letter: "3",
-    color: "#F06543",
+    color: "#FEF723",
   },
   {
     letter: "4",
-    color: "#F06543",
+    color: "#B5D444",
   },
   {
     letter: "5",
-    color: "#F06543",
+    color: "#0AADE8",
   },
   {
     letter: "6",
-    color: "#F06543",
+    color: "#EA259D",
   },
   {
     letter: "7",
-    color: "#F06543",
+    color: "#363190",
   },
   {
     letter: "8",
-    color: "#F06543",
+    color: "#F9403C",
   },
   {
     letter: "9",
-    color: "#F06543",
+    color: "#F5901E",
   },
   {
     letter: "10",
-    color: "#F06543",
+    color: "#FFF11A",
   },
   {
     letter: "11",
-    color: "#F06543",
+    color: "#B7D340",
   },
   {
     letter: "12",
-    color: "#F06543",
+    color: "#0AADE9",
   },
   {
     letter: "13",
-    color: "#F06543",
+    color: "#EA008D",
   },
   {
     letter: "14",
-    color: "#F06543",
+    color: "#2E2B92",
   },
   {
     letter: "15",
-    color: "#F06543",
+    color: "#F6A01A",
   },
   {
     letter: "16",
-    color: "#F06543",
+    color: "#F59120",
   },
   {
     letter: "17",
-    color: "#F06543",
+    color: "#F8EA13",
   },
   {
     letter: "18",
-    color: "#F06543",
+    color: "#B3D43A",
   },
   {
     letter: "19",
-    color: "#F06543",
+    color: "#04ACE4",
   },
   {
     letter: "20",
-    color: "#F06543",
+    color: "#D9077D",
   },
   {
     letter: "21",
-    color: "#F06543",
+    color: "#2E2E8C",
   },
   {
     letter: "22",
-    color: "#F06543",
+    color: "#ED3131",
   },
   {
     letter: "23",
-    color: "#F06543",
+    color: "#F7901B",
   },
   {
     letter: "24",
-    color: "#F06543",
+    color: "#F9F506",
   },
   {
     letter: "25",
-    color: "#F06543",
+    color: "#B7D64A",
   },
   {
     letter: "26",
-    color: "#F06543",
+    color: "#02AFE8",
   },
   {
     letter: "27",
@@ -109,14 +109,14 @@ export const numbers = [
   },
   {
     letter: "28",
-    color: "#F06543",
+    color: "#F99B28",
   },
   {
     letter: "29",
-    color: "#F06543",
+    color: "#FEF723",
   },
   {
     letter: "30",
-    color: "#F06543",
+    color: "#B5D444",
   },
 ];
