c0fb0016cf93454b20cabc83fb7846c8b2edaf66
yurii-serdiuk-gaming <yurii.serdiuk@authenticgaming.com>
2021-08-30 00:07:58 +0200

    added server

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const cors = require("cors");
+const path = require("path");
+const PORT = process.env.PORT || 4000;
+
+app.use(cors());
+app.use(express.static(__dirname + '/build'));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "build", "index.html"));
+});
+
+async function start() {
+  try {
+    app.listen(PORT,()=>{
+      console.log(`server started on port ${PORT}`);
+    }) ;
+  }catch (e) {
+    console.log(e);
+  }
+}
+
+start();
